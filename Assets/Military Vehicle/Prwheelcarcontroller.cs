7aaff3e0ab97a3180e3785b87ef89093121eed67
h1ght0wer <pedersen.lasse87@gmail.com>
2021-01-17 20:11:30 +0100

    First upload

---
+++ Assets/Military Vehicle/Prwheelcarcontroller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Prwheelcarcontroller : MonoBehaviour
+{
+
+    private float m_horizontalInput;
+    private float m_verticalInput;
+    private float m_steeringAngle;
+
+    public WheelCollider FL_Wheel, FR_Wheel;
+    public WheelCollider BL_Wheel, BR_Wheel;
+    public Transform FL_WheelT, FR_WheelT;
+    public Transform BL_WheelT, BR_WheelT;
+    public float maxSteeringAngle = 30;
+    public float motorForce = 50; 
+
+    private void GetInput()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        m_steeringAngle = maxSteeringAngle * m_horizontalInput;
+        FL_Wheel.steerAngle = m_steeringAngle;
+        FR_Wheel.steerAngle = m_steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        BR_Wheel.motorTorque = m_verticalInput * motorForce;
+        BL_Wheel.motorTorque = m_verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(FL_Wheel, FL_WheelT);
+        UpdateWheelPose(FR_Wheel, FR_WheelT);
+        UpdateWheelPose(BL_Wheel, BL_WheelT);
+        UpdateWheelPose(BR_Wheel, BR_WheelT);
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
---
+++ Assets/SCripts/Carcontroller.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Carcontroller : MonoBehaviour
+{
+
+    public Rigidbody theRB;
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragGround = 3f;
+
+    private float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.5f;
+    public Transform groundRayPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+        transform.position = theRB.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit; 
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+
+        if (grounded)
+        {
+            theRB.drag = dragGround;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+
+                theRB.AddForce(transform.forward * speedInput);
+            }
+        }
+        else
+        {
+            theRB.drag = 0.1f;
+
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
+        }
+    }
+}
